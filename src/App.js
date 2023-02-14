8835fa3209db5cdf5bf8b14ec407ecdbaaa457ad
DukeM23 <dukemaquiling@gmail.com>
2023-02-13 23:34:43 -0500

    sending to main

--- src/App.js
+++ src/App.js
@@ -22,9 +22,6 @@ import GenreSelection from "./components/GenreSelection";
 export const ResultContext = React.createContext();
 export const TokenContext = React.createContext()
 
-// dotenv.config();
-
-
 
 function App() {
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
