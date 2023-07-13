9f4735845aa310ea3d3f2dc77efb028033522fd5
Duke D.M. Maquiling <duke maquiling@rutherfordglobal.biz>
2023-07-13 12:28:51 -0400

    Fixed ENV variables

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -6,8 +6,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-const REDIRECT_URI = "https://randomify-git-genre-selection-dukem23.vercel.app/genre-selection/"
-// const REDIRECT_URI = "http://localhost:3000/genre-selection"
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 const AUTH_ENDPOINT = process.env.REACT_APP_AUTH_ENDPOINT;
 const RESPONSE_TYPE = process.env.REACT_APP_RESPONSE_TYPE;
 const SCOPE = process.env.REACT_APP_SCOPE;
